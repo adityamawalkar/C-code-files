b9938551372c7ad4eea2f5abfb7b10f68c50f261
Aditya Mawalkar <121755394+adityamawalkar@users.noreply.github.com>
2023-10-22 15:12:13 +0530

    5th file added

---
+++ Square addition.c
@@ -0,0 +1,13 @@
+main()
+{
+  int a,b;
+  clrscr();
+  printf("enter the value of a=");
+  scanf("%d",&a);
+  printf("enter the value of b=");
+  scanf("%d",&b);
+  printf("square of a = %d\n",a*a);
+  printf("square of b = %d\n",b*b);
+  printf("sum of %d and %d is %d",a*a,b*b,a*a+b*b);
+  getch(); 
+}
